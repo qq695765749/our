9b6177d10cd3ab9793e3dfcd4c91b3d87281775e
tom <tom@qq.com>
2018-10-19 11:41:52 +0800

    git+autoPull

---
+++ application/local/controller/git.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function autoPull()
+    {
+    	$local = '/var/www/html/our';
+    	if (!is_dir($local)) {
+    		header('HTTP/1.1 500 Internal Server Error');
+    		die('Local directory is missing');
+    	}
+    	
+    	$payload = file_get_contents('php://input');
+    	if (!$payload) {
+    		header('HTTP/1.1 400 Bad Request');
+    		die('HTTP HEADER or POST is missing.');
+    	}
+    	
+    	
+    	echo shell_exec("cd {$local} && git pull 2>&1");
+    	die("done " . date('Y-m-d H:i:s', time()));
+    }
+
+  
+}
